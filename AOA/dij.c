e738525fdfa25c8c98f8feceec0c7e1c9ec2c01f
Kevlyn Kadamala <43489154+kad99kev@users.noreply.github.com>
2019-02-26 19:41:09 +0530

    Add files via upload

---
+++ AOA/dij.c
@@ -0,0 +1,246 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define SIZE 10
+
+typedef struct queue
+{
+	int a[SIZE];
+	int front;
+	int rear;
+}que;
+
+int adj[SIZE][SIZE]={0};
+
+void create_graph(int n)
+{
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j < n; j++){
+      adj[i][j] = 1000;
+    }
+  }
+	int s, d, e, i, j, w;
+	for(i=0; i<n; i++)
+	{
+		printf("Enter number edges for %d\n", i);
+		scanf("%d", &e);
+		for(j=0; j<e; j++)
+		{
+      printf("Enter destination\n");
+			scanf("%d", &d);
+      printf("Enter weight\n");
+      scanf("%d", &w);
+			adj[i][d]=w;
+		}
+	}
+}
+
+void display_adjmat(int n)
+{
+    int i,j;
+    printf("Adjacency matrix is\n");
+           for(i=0; i<n; i++)
+           {
+               for(j=0; j<n; j++)
+               {
+                   printf(" %d ", adj[i][j]);
+               }
+               printf("\n");
+           }
+}
+
+void test_graph1(int n){
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j < n; j++){
+      adj[i][j] = 1000;
+    }
+  }
+  adj[0][1] = 20;
+  adj[0][2] = 15;
+  adj[1][0] = 2;
+  adj[1][5] = 30;
+  adj[1][4] = 10;
+  adj[2][5] = 10;
+  adj[2][3] = 4;
+  adj[4][3] = 15;
+  adj[5][3] = 4;
+  adj[5][4] = 10;
+}
+
+void test_graph2(int n){
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j < n; j++){
+      adj[i][j] = 1000;
+    }
+  }
+  adj[0][1] = 2;
+  adj[0][2] = 8;
+  adj[1][2] = 5;
+  adj[1][3] = 3;
+  adj[2][1] = 6;
+  adj[2][4] = 6;
+  adj[3][2] = 1;
+  adj[3][4] = 7;
+  adj[3][5] = 6;
+  adj[4][3] = 4;
+  adj[5][4] = 2;
+}
+
+void disp_arr(int arr[], int n, int src){
+  int i;
+  for(i = 0; i < n; i++){
+		if(i == src){
+			printf("src ");
+		}
+		else{
+			printf("%d ", arr[i]);
+		}
+  }
+  printf("\n");
+}
+
+void display(int pred[], int dist[], int n, int src){
+  int i, j;
+  printf("\nDistance array is\n");
+  disp_arr(dist, n, src);
+  printf("Predecessor array is\n");
+  disp_arr(pred, n, src);
+  printf("\n");
+  for(i = 0; i < n; i++){
+    if(i == src){
+      printf("%d is source node\n", i);
+      continue;
+    }
+    printf("\nFor vertex %d\n", i);
+    printf("%d ", i);
+    j = i;
+    while(pred[j] != src){
+      j = pred[j];
+      printf("<-%d ", j);
+    }
+    printf("<-%d ", src);
+    printf("\n");
+    printf("Total distance is %d\n", dist[i]);
+  }
+}
+
+void dij(int n){
+  int src, newsrc, n1, n2, min, check = 1, i;
+  int dist[SIZE] = {1000};
+  int pred[SIZE] = {0};
+  int visited[SIZE] = {0};
+  printf("Enter source node\n");
+  scanf("%d", &src);
+  visited[src] = 1;
+  for(i = 0; i < n; i++){
+    if(i != src){
+      dist[i] = adj[src][i];
+      pred[i] = src;
+    }
+  }
+  while(check){
+    min = 1000;
+    for(i = 0; i < n; i++){
+      if(visited[i] == 0 && min > dist[i]){
+        min = dist[i];
+        newsrc = i;
+      }
+    }
+    visited[newsrc] = 1;
+
+    for(i = 0; i < n; i++){
+
+      if(adj[newsrc][i] != 1000 && i != src){
+        n1 = dist[i];
+        n2 = dist[newsrc] + adj[newsrc][i];
+
+        if(n2 < n1){
+          dist[i] = n2;
+          pred[i] = newsrc;
+        }
+      }
+    }
+    for(i = 0; i < n; i++){
+      if(visited[i] == 1){
+        check = 0;
+      }
+      else{
+        check = 1;
+				break;
+      }
+    }
+  }
+  display(pred, dist, n, src);
+}
+
+void main(){
+  int n;
+  printf("Enter number of vertices\n");
+  scanf("%d", &n);
+  create_graph(n);
+  dij(n);
+}
+
+/*
+First test
+Enter source node
+0
+
+Distance array is
+1000 20 15 19 30 25
+Predecessor array is
+0 0 0 2 1 2
+
+0 is source node
+
+For vertex 1
+1 <-0
+Total distance is 20
+
+For vertex 2
+2 <-0
+Total distance is 15
+
+For vertex 3
+3 <-2 <-0
+Total distance is 19
+
+For vertex 4
+4 <-1 <-0
+Total distance is 30
+
+For vertex 5
+5 <-2 <-0
+Total distance is 25
+
+
+Second test
+Enter source node
+0
+
+Distance array is
+1000 2 6 5 12 11
+Predecessor array is
+0 0 3 1 3 3
+
+0 is source node
+
+For vertex 1
+1 <-0
+Total distance is 2
+
+For vertex 2
+2 <-3 <-1 <-0
+Total distance is 6
+
+For vertex 3
+3 <-1 <-0
+Total distance is 5
+
+For vertex 4
+4 <-3 <-1 <-0
+Total distance is 12
+
+For vertex 5
+5 <-3 <-1 <-0
+Total distance is 11
+*/
